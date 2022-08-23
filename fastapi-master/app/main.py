92f69ba1204459b3dbfc906a6ded39c0ae4a6eda
Tiago Marante <1200627@isep.ipp.pt>
2022-08-23 18:07:42 +0100

    some changes + docs

--- fastapi-master/app/main.py
+++ fastapi-master/app/main.py
@@ -10,7 +10,7 @@ from app.models.email import Email
 from app.utils.utils import to_json
 from app.utils.word_analysis import *
 
-model_load = joblib.load("../fastapi-master/app/ML model/trainedForest.joblib")
+model = joblib.load("../fastapi-master/app/ML model/trainedForest.joblib")
 
 app = FastAPI()
 
@@ -23,5 +23,5 @@ async def get_health_status():
 @app.post("/email/")
 async def post_email(email: Email):
     content = email.email
-    res = email_analysis(content, model_load)
+    res = email_analysis(content, model)
     return to_json(res)
--- fastapi-master/entrypoint.sh
+++ fastapi-master/entrypoint.sh
@@ -2,5 +2,6 @@
 set -e
 
 #. /venv/bin/activate
+#http://0.0.0.0:8001/docs#/
 
 gunicorn app.main:app -b localhost:8001 -k uvicorn.workers.UvicornWorker --workers 4
